f94f5e069797ba744a56f853a9baa4c268e84d10
Yuri <yurivictor4525@gmail.com>
2021-08-04 00:52:33 -0300

    Java LINT

--- src/Fundamentos/Basico.java
+++ src/Fundamentos/Basico.java
@@ -1,17 +1,18 @@
 package Fundamentos;
+
 import java.util.Scanner;
 
 public class Basico {
 	public static void main(String[] args) {
-		 	System.out.println("Bom dia!");
-	        
-	        System.out.printf("O resltado da megaena é: %d %d %d %d %n ", 4, 6, 8, 9  );
-	        
-	        Scanner entrada = new Scanner(System.in);
-	        
-	        System.out.println("Digite seu nome:  ");
-	        String nome = entrada.nextLine();
-	        
-	        System.out.println("\n\n Nome:  " + nome);
+		System.out.println("Bom dia!");
+
+		System.out.printf("O resltado da megaena é: %d %d %d %d %n ", 4, 6, 8, 9);
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Digite seu nome:  ");
+		String nome = entrada.nextLine();
+
+		System.out.println("\n\n Nome:  " + nome);
 	}
 }
