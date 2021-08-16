8297976e4b3421a0ed1ea847dd6963f935e7bc1a
Yuri <yurivictor4525@gmail.com>
2021-08-16 19:30:58 -0300

    For
    Exemplo for

---
+++ src/Controle/WileDeterminado.java
@@ -0,0 +1,13 @@
+package Controle;
+
+public class WileDeterminado {
+
+	public static void main(String[] args) {
+		int contador = 1;
+		
+		while (contador <=20) {
+			System.out.println("Bom dia!!");
+			contador++;
+		}
+	}
+}
